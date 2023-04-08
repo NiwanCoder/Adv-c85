df9c213a9a13b728a6678c9669026ba040348de5
Niwan Raj Bhati <86115960+NiwanCoder@users.noreply.github.com>
2023-04-08 08:18:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,166 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+
+greencar_width=75;
+greencar_height=100;
+
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+
+
+greencar_x=430;
+greencar_y=89;
+
+function add() {
+  background_imgTag=new Image();
+  background_imgTag.onload=uploadBackground;
+  background_imgTag.src=background_image
+
+  greencar_imgTag=new Image();
+  greencar_imgTag.onload=uploadgreencar;
+  greencar_imgTag.src=greencar_image;
+}
+
+function uploadBackground() {
+	ctx.drawImage(background_imgTag,0,0,canvas.width,canvas.height);
+}
+
+function uploadgreencar() {
+    ctx.drawImage(greencar_imgTag,greencar_x,greencar_y,greencar_width,greencar_height);
+}
+
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		if(keyPressed == '87')	
+		{
+			w();
+			console.log("w");
+		}
+		if(keyPressed == '83')
+		{
+		   s();
+			console.log("s");
+		}
+		if(keyPressed == '65')
+		{
+		   a();
+			console.log("a");
+		}
+		if(keyPressed == '68')
+		{
+		   d();
+			console.log("d");
+		}
+		
+		
+		
+		
+		
+}
+
+function up()
+{
+	if(greencar_y >=0){
+		greencar_y=greencar_y -10;
+		console.log("when up arrow is pressed, x="+greencar_x+"| y="+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+
+}
+
+function down()
+{
+	if(greencar_y <=500){
+		greencar_y +=10;
+		console.log("when down arrow is pressed, x="+greencar_x+"| y="+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
+
+function left()
+{
+	if(greencar_x >=0){
+		greencar_x-=10;
+		console.log("when left arrow is pressed, x="+greencar_x+"| y="+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
+
+function right()
+{
+	if(greencar_x <=700){
+		greencar_x +=10;
+		console.log("when right arrow is pressed, x="+greencar_x+"| y="+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
+function w()
+{
+	if(greencar_y >=0){
+		greencar_y=greencar_y -10;
+		console.log("when up arrow is pressed, x="+greencar_x+"| y="+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
+
+function s()
+{
+	if(greencar_y <=500){
+		greencar_y +=10;
+		console.log("when down arrow is pressed, x="+greencar_x+"| y="+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
+function a()
+{
+	if(greencar_x >=0){
+		greencar_x-=10;
+		console.log("when left arrow is pressed, x="+greencar_x+"| y="+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
+function d()
+{
+	if(greencar_x <=700){
+		greencar_x +=10;
+		console.log("when right arrow is pressed, x="+greencar_x+"| y="+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
